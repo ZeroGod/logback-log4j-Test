32ccce03232b565a3f0ed17ad1e04acf7c5ea98f
zero <shanghouxiong@live.cn>
2017-07-19 15:46:37 +0800

    logback test

---
+++ src/main/java/Test.java
@@ -0,0 +1,13 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Logger logger = LoggerFactory.getLogger(Test.class);
+		logger.info("1111");
+		System.out.println("test");
+
+	}
+
+}
